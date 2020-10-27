b039dc80dff70887a5966398a5442b5a3e5ec4e8
jsksru <mail@jsks.ru>
2020-10-27 21:25:32 +0500

    output feature

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,7 +1,10 @@
-const Result = ({ html }) => {
+const Result = ({ html, css, js }) => {
+
+  const htmlCode = `<html><head><style>${css}</style></head><body>${html}<script>${js}</script></body></html>`;
+
   return (
     <div className="field result-box">
-      {html}
+      <iframe title="result" srcDoc={htmlCode} className="output"></iframe>
     </div>
   );
 };
