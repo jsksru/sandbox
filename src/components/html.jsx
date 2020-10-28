d1fb033726548d2a8e2d10871619027894b4fb6e
jsksru <mail@jsks.ru>
2020-10-28 14:30:20 +0500

    add ace code aditor

--- src/components/html.jsx
+++ src/components/html.jsx
@@ -1,8 +1,31 @@
+import AceEditor from 'react-ace';
+
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/theme-monokai";
+
 const Html = ({ code, handler }) => {
   return (
     <div className="field html-box">
       <div className="title">HTML</div>
-      <textarea className="input" value={code} onChange={(e) => handler(e.target.value)}></textarea>
+      <AceEditor
+        className="input"
+        value={code}
+        onChange={(value) => handler(value)}
+        mode="html"
+        theme="monokai"
+        fontSize={16}
+        showGutter={true}
+        highlightActiveLine={true}
+        name="htmlcodebox"
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+      />
     </div>
   );
 };
